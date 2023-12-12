dbc96afc3b727005e4babd4d4d45a02b5a3f9ccb
preangeleo <preangelleo@gmail.com>
2023-12-12 10:03:08 -0800

    daily, weekly and monthly chart

--- BTC_weekly.py
+++ BTC_weekly.py
@@ -67,7 +67,7 @@ def get_btc_data_with_rsi(timeframe='1w', chat_id=None):
     # get the price of BTC
     btc_price = df['close'].iloc[-1]
 
-    if chat_id: send_img(chat_id, file_name, f'Current Price: {btc_price:.2f} usdt')
+    if chat_id: send_img(chat_id, file_name, f'{datetime.now().strftime("%Y-%m-%d")} | {btc_price:.2f} usdt/btc')
     return file_name
 
 
