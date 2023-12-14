13cfd58da1791f7767ec0c174b1a7369296bfc59
preangeleo <preangelleo@gmail.com>
2023-12-13 18:16:11 -0800

    Flask webhook

--- Flask_api.py
+++ Flask_api.py
@@ -1,54 +1,35 @@
 from Top_functions import *
+from flask import Flask
 
-# Flask app and API initialization
 app = Flask(__name__)
 
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-# Home route
-@app.route('/')
-def home():
-    return """
-    <html>
-    <body style="text-align: center;">
-        <h1>LEOWANG.NET</h1>
-    </body>
-    </html>
-    """
+# # Create a webhook to receive messages from Tradingview
+# @app.route('/tv', methods=['POST'])
+# def tv():
+#     # Get the json data
+#     data = request.json
 
-# Home route
-@app.route('/test')
-def test():
-    return """
-    <html>
-    <body style="text-align: center;">
-        <h1>LEOWANG.NET</h1>
-    </body>
-    </html>
-    """
+#     print(data)
+#     '''{"condition": "P", "message": "1234567"}'''
 
-# Create a webhook to receive messages from Tradingview
-@app.route('/tv', methods=['POST'])
-def tv():
-    # Get the json data
-    data = request.json
+#     try:
+#         # Extract the message
+#         message = data.get('message', 'None')
+#         condition = data.get('condition', 'None').upper()
+#     except: return {'message': 'Invalid data'}, 200
 
-    print(data)
-    '''{"condition": "P", "message": "1234567"}'''
+#     trading_bot_status = trading_bot_switch_status()
 
-    try:
-        # Extract the message
-        message = data.get('message', 'None')
-        condition = data.get('condition', 'None').upper()
-    except: return {'message': 'Invalid data'}, 200
+#     if not trading_bot_status and condition == 'P': webhook_switch_on_bot(message, TG_BOT_OWNER_ID)
+#     elif trading_bot_status and condition == 'N': webhook_switch_off_bot(message, TG_BOT_OWNER_ID)
 
-    trading_bot_status = trading_bot_switch_status()
-
-    if not trading_bot_status and condition == 'P': webhook_switch_on_bot(message, TG_BOT_OWNER_ID)
-    elif trading_bot_status and condition == 'N': webhook_switch_off_bot(message, TG_BOT_OWNER_ID)
-
-    return {'message': 'Thanks'}, 200
+#     return {'message': 'Thanks'}, 200
 
 
 # Run the application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
