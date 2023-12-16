272a430d62a30f12463f49e35ecbbaa150da110a
preangeleo <preangelleo@gmail.com>
2023-12-16 12:36:38 -0800

    reset limit orders when got OFF signal from webhook

--- Binance_api.py
+++ Binance_api.py
@@ -2375,7 +2375,23 @@ def check_and_buy_bnb(coin = 'BNB', check_limit = 1, chat_id=TG_BOT_OWNER_ID):
     
     return
     
-    
+# define a function to switch on the trading bot and send a message to the user
+def webhook_switch_on_bot(msg = 'None', from_id=TG_BOT_OWNER_ID):
+    if trading_bot_switch_on(): 
+        target_profit = float(os.getenv('TARGET_PROFIT', 0.05))
+        set_target_profit_default(target_profit)
+        return send_msg(f"TradingView webhook has switched ON the bot!\nTarget profit also set back to: {target_profit * 100:.2f}%\nCommand to reset target profit:\n\n/set_target_profit 0.07\n\nMessage from TradingView:\n{msg}", from_id)
+    return send_msg(f"TradingView webhook failed to switch on the bot! Message from TradingView:\n{msg}", from_id)
+
+
+# define a function to switch off the trading bot and send a message to the user
+def webhook_switch_off_bot(msg = 'None', from_id=TG_BOT_OWNER_ID):
+    if trading_bot_switch_off(): 
+        binance_position_reset_limit_sell(coin = None, target_profit = 0.01, transactTime = 1, from_id = TG_BOT_OWNER_ID)
+        return send_msg(f"TradingView webhook has switched OFF the bot! And reset all limit sell orders to 1% target profit for positions older than 1 day.\n\nMessage from TradingView:\n{msg}", from_id)
+    return send_msg(f"TradingView webhook failed to switch off the bot!\n\nMessage from TradingView:\n{msg}", from_id)
+
+
 if __name__ == '__main__':
     print('Binance_api.py is running')
 
