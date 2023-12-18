d4bd95e86d86931a420eaea96f6cdfce7e6a44ec
preangeleo <preangelleo@gmail.com>
2023-12-18 09:57:16 -0800

    RESET_TELEGRAM_TOKEN and delete message

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
 from Tradingview_handler import *
 
 app = Flask(__name__)
-JUST_STARTED = True
-UPDATE_ID = int(os.getenv('UPDATE_ID'))
 
 @app.route("/")
 def hello_world():
@@ -26,43 +24,32 @@ def tv_webhook():
 @app.route(f'/{TELEGRAM_BOT_WEBHOOK_TOKEN}', methods=['POST'])
 def tg_webhook():
     try:
-        global JUST_STARTED
-        global UPDATE_ID
-        print(f"PREVIOUS UPDATE_ID: {UPDATE_ID}, JUST_STARTED: {JUST_STARTED}")
-
-        if JUST_STARTED: send_msg(f"This is the first message after the bot was restarted.", TG_BOT_OWNER_ID)
-
         # Get the message from Telegram
         update = request.get_json()
 
         if update:
-            print(json.dumps(update, indent=2))
+            # print(json.dumps(update, indent=2))
 
             update_id = update['update_id']
-            update_id = int(update_id)
 
             if 'message' in update:
 
-                if not JUST_STARTED and update_id != UPDATE_ID + 1: 
-                    latest_message_dict = get_latest_message_from_telegram_messages_table()
+                latest_message_dict = get_latest_message_from_telegram_messages_table()
 
-                    if update_id != latest_message_dict['update_id'] + 1: 
-                        try: 
-                            if str(update['message']['text']) != str(RESET_TELEGRAM_TOKEN) or int(update['message']['from']['id']) != int(TG_BOT_OWNER_ID): return jsonify({'status': 'success'})
-                            else:
-                                message_id = update['message']['message_id']
-                                try: delete_msg(TG_BOT_OWNER_ID, message_id)
-                                except: pass 
-                                send_msg(f"Telegram bot was reset successfully.", TG_BOT_OWNER_ID)
-                        except: return jsonify({'status': 'success'})
+                if update_id != latest_message_dict['update_id'] + 1: 
+                    try: 
+                        if update['message']['text'] != RESET_TELEGRAM_TOKEN or update['message']['from']['id'] != TG_BOT_OWNER_ID: return jsonify({'status': 'success'})
+                        else:
+                            message_id = update['message']['message_id']
+                            try: delete_msg(TG_BOT_OWNER_ID, message_id)
+                            except: pass 
 
-                UPDATE_ID = update_id
-                JUST_STARTED = False
+                            send_msg(f"Telegram bot was reset successfully.", TG_BOT_OWNER_ID)
 
-                print(f"CURRENT UPDATE_ID: {UPDATE_ID}, JUST_STARTED: {JUST_STARTED}")
+                    except: return jsonify({'status': 'success'})
 
-                try: r = insert_telegram_message_from_webhook(update)
-                except: pass
+                try: insert_telegram_message_from_webhook(update)
+                except Exception as e: print(f"ERROR while insert_telegram_message_from_webhook(): \n\n{e}\n\n")
 
                 message = update['message']
                 chat_id = message['chat']['id']
