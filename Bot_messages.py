eaba1480c83350751f918a065bc536e9e4e073aa
preangeleo <preangelleo@gmail.com>
2023-12-19 19:38:07 -0800

    allow team members to use otp

--- Bot_messages.py
+++ Bot_messages.py
@@ -315,7 +315,7 @@ def handel_telegram_message_from_webhook_non_owner(message):
 
     text_prompt = text_prompt.replace('/', '')
 
-    COMMANDS_LIST_TOTAL = NONE_PARAMETER_COMMAND_LIST.keys() + ONE_PARAMETER_COMMAND_LIST.keys() + TWO_PARAMETER_COMMAND_LIST.keys() + THREE_PARAMETER_COMMAND_LIST.keys() + FOUR_PARAMETER_COMMAND_LIST.keys() + SENTENCE_AS_PARAMETER_COMMAND_LIST.keys()
+    COMMANDS_LIST_TOTAL = list(NONE_PARAMETER_COMMAND_LIST.keys()) + list(ONE_PARAMETER_COMMAND_LIST.keys()) + list(TWO_PARAMETER_COMMAND_LIST.keys()) + list(THREE_PARAMETER_COMMAND_LIST.keys()) + list(FOUR_PARAMETER_COMMAND_LIST.keys()) + list(SENTENCE_AS_PARAMETER_COMMAND_LIST.keys())
 
     command_word = text_prompt.lower().split()[0].lower()
     command_word = BOT_COMMAND_DICT.get(command_word, command_word)
