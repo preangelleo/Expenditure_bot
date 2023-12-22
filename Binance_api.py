31cebf12c685d4d40c3e1ee2c228d6e652f2a626
preangeleo <preangelleo@gmail.com>
2023-12-22 11:07:23 -0800

    details conditions

--- Binance_api.py
+++ Binance_api.py
@@ -1890,35 +1890,30 @@ def analyze_data(df, sma_period, rsi_period, interval, coin, from_id=None):
     latest = df.iloc[-1]
     previous = df.iloc[-2]
 
-    if latest['Close'] < latest['SMA']: 
-        print(f"{coin} {interval} close price {latest['Close']} is below SMA {latest['SMA']}")
-        if from_id: send_msg(f"{coin} {interval} interval close price {format_number(latest['Close'])} is below SMA {format_number(latest['SMA'])}", from_id)
-        return False
-    
     # Check if latest sma is lower than previous sma
-    if latest['SMA'] < previous['SMA']: 
+    if latest['SMA'] < previous['SMA'] and interval in ['5m', '15m']: 
         print(f"{coin} {interval} SMA {latest['SMA']} is below previous SMA {previous['SMA']}")
         if from_id: send_msg(f"{coin} {interval} interval SMA {format_number(latest['SMA'])} is below previous SMA {format_number(previous['SMA'])}", from_id)
         return False
     
-    if latest['RSI'] < latest['RSI_SMA']: 
+    if latest['RSI'] < latest['RSI_SMA'] and interval in ['4h', '1h']: 
         print(f"{coin} {interval} RSI {latest['RSI']} is below RSI_SMA {latest['RSI_SMA']}")
         if from_id: send_msg(f"{coin} {interval} interval RSI {format_number(latest['RSI'])} is below RSI_SMA {format_number(latest['RSI_SMA'])}", from_id)
         return False
     
     # check if latest rsi is lower than previous rsi
-    if latest['RSI'] < previous['RSI']:
+    if latest['RSI'] < previous['RSI'] and interval in ['4h', '1h', '15m', '5m']:
         print(f"{coin} {interval} RSI {latest['RSI']} is below previous RSI {previous['RSI']}")
         if from_id: send_msg(f"{coin} {interval} interval RSI {format_number(latest['RSI'])} is below previous RSI {format_number(previous['RSI'])}", from_id)
         return False
     
-    if latest['Open'] < previous['Close']: 
+    if latest['Open'] < previous['Close'] and interval in ['15m', '5m']: 
         print(f"{coin} {interval} open price {latest['Open']} is lower than previous close price {previous['Close']}")
         if from_id: send_msg(f"{coin} {interval} interval open price {format_number(latest['Open'])} is lower than previous close price {format_number(previous['Close'])}", from_id)
         return False
     
-    # check if thequote asset volume is higher than average quote asset volume
-    if latest['Quote Asset Volume'] < latest['Quote Asset Volume SMA']:
+    # check if the quote asset volume is higher than average quote asset volume
+    if latest['Quote Asset Volume'] < latest['Quote Asset Volume SMA'] and previous['Quote Asset Volume'] < previous['Quote Asset Volume SMA']:
         print(f"{coin} {interval} interval trading volume {latest['Quote Asset Volume']} is below SMA {latest['Quote Asset Volume SMA']}")
         if from_id: send_msg(f"{coin} {interval} interval trading volume {format_number(latest['Quote Asset Volume'])} is below SMA {format_number(latest['Quote Asset Volume SMA'])}", from_id)
         return False
@@ -1942,7 +1937,9 @@ def analyze_symbol(symbol: str, from_id=None):
     print(f"Finished analyzing {symbol[:-4]}, and it is good to buy now.")
     return True
 
-
+symbol = 'CAKE'
+from_id = TG_BOT_OWNER_ID
+analyze_symbol(symbol, from_id)
 
 # Define a function to check if the weekly interval rsi is over 90, if yes, remove from white_list
 def weekly_rsi_over_high(symbol, from_id=TG_BOT_OWNER_ID):
