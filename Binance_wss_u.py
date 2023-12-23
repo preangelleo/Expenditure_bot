1a9f99e3b954a8bbef4b6a8dce969aa9328f41d8
preangeleo <preangelleo@gmail.com>
2023-12-22 23:03:28 -0800

    Binance_wss_u.py

--- Binance_wss_u.py
+++ Binance_wss_u.py
@@ -23,18 +23,27 @@ def on_message(ws, message):
         if data['o']['S'] == 'SELL' and data['o']['ps'] == 'SHORT':
             try: 
                 send_msg(f"UMFUTURE {data['o']['ps']} SHORT at {data['o']['ap']}", TG_BOT_OWNER_ID)
-                # 将新订单json转换成 dataframe 并写入table 'umfuture_short_orders', append
-                df = pd.DataFrame(data['o'], index=[0])
-                df.to_sql('umfuture_short_orders', engine, if_exists='append', index=False)
+                # 将新订单json转换成 dataframe 并写入table 'umfuture_orders_short', append
+                new_data = {
+                    'coin': data['o']['s'][:-4],
+                    'symbol': data['o']['s'],
+                    'price': data['o']['ap'],
+                    'direction': 'SHORT',
+                    'type': 'SELL',
+                    'trade_time': data['o']['T'],
+                    'trade_id': data['o']['c']
+                }
+                df = pd.DataFrame(new_data, index=[0])
+                df.to_sql('umfuture_orders_short', engine, if_exists='append', index=False)
             except Exception as e: print(f"UMFUTURE Send Message Error: \n\n{e}\n\n")
 
         if data['o']['S'] == 'BUY' and data['o']['ps'] == 'SHORT':
             try:
-                df = pd.DataFrame(engine.connect().execute(text('SELECT * FROM umfuture_short_orders WHERE s = :s AND S = :S ORDER BY T DESC LIMIT 1'), {'s': data['o']['s'], 'S': 'SELL'}).fetchall())
+                df = pd.DataFrame(engine.connect().execute(text('SELECT * FROM umfuture_orders_short WHERE coin = :coin AND type = :type ORDER BY trade_time DESC LIMIT 1'), {'coin': data['o']['s'][:-4], 'type': 'SELL'}).fetchall())
                 if not df.empty:
                     coin = data['o']['s'][:-4]
-                    price_change = float(df['ap']) - float(data['o']['ap'])
-                    profit = price_change * float(df['q']) * 10000
+                    price_change = float(df['price']) - float(data['o']['ap'])
+                    profit = price_change * 10000
                     profit = format_number(profit)
                     send_msg(f"UMFUTURE {coin} SHORT closed >> {profit} usdt", TG_BOT_OWNER_ID)
             except: pass
@@ -42,63 +51,31 @@ def on_message(ws, message):
         if data['o']['S'] == 'BUY' and data['o']['ps'] == 'LONG':
             try: 
                 send_msg(f"UMFUTURE {data['o']['ps']} LONG at {data['o']['ap']}", TG_BOT_OWNER_ID)
-                # 将新订单json转换成 dataframe 并写入table 'umfuture_long_orders', append
-                df = pd.DataFrame(data['o'], index=[0])
-                df.to_sql('umfuture_long_orders', engine, if_exists='append', index=False)
+                # 将新订单json转换成 dataframe 并写入table 'umfuture_orders_long', append
+                new_data = {
+                    'coin': data['o']['s'][:-4],
+                    'symbol': data['o']['s'],
+                    'price': data['o']['ap'],
+                    'direction': 'LONG',
+                    'type': 'BUY',
+                    'trade_time': data['o']['T'],
+                    'trade_id': data['o']['c']
+                }
+                df = pd.DataFrame(new_data, index=[0])
+                df.to_sql('umfuture_orders_long', engine, if_exists='append', index=False)
             except Exception as e: print(f"UMFUTURE Send Message Error: \n\n{e}\n\n")
         
         if data['o']['S'] == 'SELL' and data['o']['ps'] == 'LONG':
             try:
-                df = pd.DataFrame(engine.connect().execute(text('SELECT * FROM umfuture_long_orders WHERE s = :s AND S = :S ORDER BY T DESC LIMIT 1'), {'s': data['o']['s'], 'S': 'BUY'}).fetchall())
+                df = pd.DataFrame(engine.connect().execute(text('SELECT * FROM umfuture_orders_long WHERE coin = :coin AND type = :type ORDER BY trade_time DESC LIMIT 1'), {'coin': data['o']['s'][:-4], 'type': 'BUY'}).fetchall())
                 if not df.empty:
                     coin = data['o']['s'][:-4]
-                    price_change = float(data['o']['ap']) - float(df['ap'])
-                    profit = price_change * float(df['q']) * 10000
+                    price_change = float(data['o']['ap']) - float(df['price'])
+                    profit = price_change * 10000
                     profit = format_number(profit)
                     send_msg(f"UMFUTURE {coin} LONG closed >> {profit} usdt", TG_BOT_OWNER_ID)
             except: pass
 
-
-''' {
-    "E": 1703309262526,
-    "T": 1703309262515,
-    "e": "ORDER_TRADE_UPDATE",
-    "o": {
-        "L": "0.11690",
-        "N": "USDT",
-        "R": true,
-        "S": "BUY",
-        "T": 1703309262515,
-        "V": "NONE",
-        "X": "FILLED",
-        "a": "0",
-        "ap": "0.11685",
-        "b": "0",
-        "c": "web_NNeR7Cr9DzP26sOr5jCr",
-        "cp": false,
-        "f": "GTC",
-        "gtd": 0,
-        "i": 3550168631,
-        "l": "307",
-        "m": false,
-        "n": "0.01794415",
-        "o": "MARKET",
-        "ot": "MARKET",
-        "p": "0",
-        "pP": false,
-        "pm": "NONE",
-        "ps": "SHORT",
-        "q": "86311",
-        "rp": "-0.33710002",
-        "s": "ROSEUSDT",
-        "si": 0,
-        "sp": "0",
-        "ss": 0,
-        "t": 149011642,
-        "wt": "CONTRACT_PRICE",
-        "x": "TRADE",
-        "z": "86311"
-    }}'''
 
 def on_error(ws, error):
     print(error)
