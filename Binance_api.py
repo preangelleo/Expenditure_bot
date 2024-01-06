7691ebb77672812b406f7dc3005c6df74a0f2a1e
preangeleo <preangelleo@gmail.com>
2024-01-06 14:02:19 -0800

    Fix funding account insufficient balance check

--- Binance_api.py
+++ Binance_api.py
@@ -3654,7 +3654,7 @@ def switch_position_from_main_to_funding(coin, from_id=TG_BOT_OWNER_ID):
     ''' binance_position_buy
         symbol    orderId  orderListId           clientOrderId   transactTime      price      origQty  executedQty cummulativeQuoteQty  status timeInForce    type side    workingTime selfTradePreventionMode  coin  buy_cost_bnb  buy_bnb_price  update_id  is_closed
     0  KP3RUSDT  299940454           -1  0Bdmn2xGbxFqNzsCITmgLw  1704165866038  117.46095  85.13000000  85.13000000       9999.45070000  FILLED         GTC  MARKET  BUY  1704165866038            EXPIRE_MAKER  KP3R      0.023503          318.0         79          0'''
-
+    if not funding_main_transfer_with_check_and_send('USDT', CHECK_SIZE, from_id): return send_msg(f'USDT in funding account is not sufficient', from_id)
     update_id = int(df['update_id'].values[0])
     # current_orders = get_open_orders_list()
     df_openorders = get_open_limit_orders(None, 'binance_limit_sell_order')
@@ -3664,7 +3664,6 @@ def switch_position_from_main_to_funding(coin, from_id=TG_BOT_OWNER_ID):
         cancel_orderId = int(df_openorders['orderId'].values[0])
         binance_cancel_order_by_orderId(coin, cancel_orderId)
         mark_limit_order_as_canceled_by_orderId(cancel_orderId)
-
     new_data = {
         'coin': coin,
         'symbol': coin + 'USDT',
@@ -3680,12 +3679,9 @@ def switch_position_from_main_to_funding(coin, from_id=TG_BOT_OWNER_ID):
         'time_string': datetime.fromtimestamp(df['transactTime'].values[0] / 1000).strftime('%Y-%m-%d %H:%M:%S'),
         'is_closed': 0,
     }
-
     if data_to_table(new_data, 'binance_funding_positions'): 
         main_funding_transfer_with_check_and_send(coin, new_data['executedQty'], from_id)
         close_position_status_by_order_id(int(new_data['orderId']))
-        funding_main_transfer_with_check_and_send('USDT', CHECK_SIZE, from_id)
-
     return send_msg(f'''{coin} position has been switched to funding account successfully!''', from_id)
 
 
