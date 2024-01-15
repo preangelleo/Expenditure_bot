3a38ed30bac16578eca3095b405cc5db2ec64261
preangeleo <preangelleo@gmail.com>
2024-01-14 23:59:52 -0800

    Fix calculation of buy price in do_market_buy function

--- Binance_api.py
+++ Binance_api.py
@@ -1776,7 +1776,8 @@ def do_market_buy(coin: str, value):
     data = binance_market_buy(coin, value)
     if not data: return f'Failed to do market buy for coin: {coin}'
     instert_position_table(data, account = 'spot')
-    return f'''Bought {coin} at {format_number(data['price'])} usdt/{coin.lower()}'''
+    price = float(data['cummulativeQuoteQty']) / float(data['executedQty'])
+    return f'''Bought {coin} at {format_number(price)} usdt/{coin.lower()}'''
 
 
 def do_market_buy_one_unit(coin: str, from_id=TG_BOT_OWNER_ID):
