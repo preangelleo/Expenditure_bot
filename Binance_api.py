4a51b4d6c2957a161151b72356c3bc10a605449d
preangeleo <preangelleo@gmail.com>
2024-01-20 22:01:44 -0800

    Update generate_bottom_msg function to remove unnecessary commands

--- Binance_api.py
+++ Binance_api.py
@@ -49,7 +49,7 @@ def network_name_change(str_name: str):
     return str_name
 
 def generate_bottom_msg(coin):
-    return f"/as_{coin} | /buy_{coin} | /mtf_{coin} | /ftm_{coin}\n/limit_sell_{coin} | /limit_buy_{coin}\n/funding_buy_{coin} | /funding_sell_{coin}\n/cpu | /gpu | /cmp | /ptt | /cpp | /cab | /cas"
+    return f"/as_{coin} | /buy_{coin} | /mtf_{coin}\n/limit_sell_{coin} | /limit_buy_{coin}\n/funding_buy_{coin} | /funding_sell_{coin}\n/cpu | /gpu | /cmp | /ptt | /cpp | /cab | /cas"
 
 def server_time_diff():
     PATH = '/api/v1/time'
