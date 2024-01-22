e68261c237867688e4752a717c3df53ad900f981
preangeleo <preangelleo@gmail.com>
2024-01-22 14:25:28 -0800

    Fix function call in Binance_api.py and update highest loss message

--- Binance_api.py
+++ Binance_api.py
@@ -1796,7 +1796,7 @@ def update_position_table(data: dict, from_id=TG_BOT_OWNER_ID):
         conn.commit()
     reply_msg = f"{data['coin']} Position closed with profit: {format_number(profit)} usdt"
     send_msg(f"{reply_msg}\n/as_{data['coin']} | /profit_taken_today", from_id)
-    get_webhook_signature_coin({data['coin']}, from_id)
+    get_webhook_signature_coin(data['coin'], from_id)
     return profit
 
 
@@ -3708,7 +3708,7 @@ def grid_profit_check_for_user(from_id=TG_BOT_OWNER_ID, grid_profit_target=1):
     df_balance = df_balance.sort_values(by='profit', ascending=True).reset_index(drop=True)
     coin_with_highest_lost = df_balance['coin'][0]
     coin_with_highest_lost_profit = format_number(df_balance['profit'][0])
-    send_msg(f"Asset: {format_number(asset_value)} usdt\nUSDT Spot: {format_number(spot_usdt)}\nUSDT Funding: {format_number(funding_usdt)}\nUSDT Total: {format_number(total_usdt)}\n\nValue Total: {format_number(total_value)} usdt\nHighest Loss: {coin_with_highest_lost} | {coin_with_highest_lost_profit}\n\n/close_postive_positions\n/close_all_positions\n/open_orders_list\n/calculate_missed_profit", from_id)
+    send_msg(f"Asset: {format_number(asset_value)} usdt\nUSDT Spot: {format_number(spot_usdt)}\nUSDT Funding: {format_number(funding_usdt)}\nUSDT Total: {format_number(total_usdt)}\n\nValue Total: {format_number(total_value)} usdt\nHighest Loss: /as_{coin_with_highest_lost} | {coin_with_highest_lost_profit}\n\n/close_postive_positions\n/close_all_positions\n/open_orders_list\n/calculate_missed_profit", from_id)
     return 
 
 
