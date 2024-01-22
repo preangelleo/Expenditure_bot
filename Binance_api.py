0af1ea0f3ee5b708703726e178201e3ac3ec3b2f
preangeleo <preangelleo@gmail.com>
2024-01-22 14:16:08 -0800

    Add highest loss coin and profit to asset message

--- Binance_api.py
+++ Binance_api.py
@@ -3704,7 +3704,11 @@ def grid_profit_check_for_user(from_id=TG_BOT_OWNER_ID, grid_profit_target=1):
     funding_usdt = data.get('funding', 0)
     total_usdt = data.get('total', 0)
     total_value = asset_value + total_usdt
-    send_msg(f"Asset: {format_number(asset_value)} usdt\nUSDT Spot: {format_number(spot_usdt)}\nUSDT Funding: {format_number(funding_usdt)}\nUSDT Total: {format_number(total_usdt)}\n\nValue Total: {format_number(total_value)} usdt\n\n/close_postive_positions\n/close_all_positions\n/open_orders_list\n/calculate_missed_profit", from_id)
+    df_balance = df_balance.groupby('coin').sum(numeric_only=True).reset_index()
+    df_balance = df_balance.sort_values(by='profit', ascending=True).reset_index(drop=True)
+    coin_with_highest_lost = df_balance['coin'][0]
+    coin_with_highest_lost_profit = format_number(df_balance['profit'][0])
+    send_msg(f"Asset: {format_number(asset_value)} usdt\nUSDT Spot: {format_number(spot_usdt)}\nUSDT Funding: {format_number(funding_usdt)}\nUSDT Total: {format_number(total_usdt)}\n\nValue Total: {format_number(total_value)} usdt\nHighest Loss: {coin_with_highest_lost} | {coin_with_highest_lost_profit}\n\n/close_postive_positions\n/close_all_positions\n/open_orders_list\n/calculate_missed_profit", from_id)
     return 
 
 
