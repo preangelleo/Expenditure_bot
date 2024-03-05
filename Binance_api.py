782333ff245156c53ba78662c1158b14053bcdf8
preangeleo <preangelleo@gmail.com>
2024-03-05 14:02:45 -0800

    Fix resistance and support level calculations in Binance_api.py

--- Binance_api.py
+++ Binance_api.py
@@ -2485,7 +2485,7 @@ def get_resistant_prices(symbol, interval = '1w'):
         current_price = float(df['Close'].iloc[-1])
         if current_price > 0:
             resistance_support_levels = calculate_resistance_support(df)
-            resistance_support_levels = [f"{format_number(price)}({format_number(100 * float(price) / current_price)}%)" for price in resistance_support_levels if price >= current_price * 1.2]
+            resistance_support_levels = [f"{format_number(price)}({format_number(100 * (float(price) - current_price) / current_price)}%)" for price in resistance_support_levels if price >= current_price * 1.2]
             # MATIC: Current Price 1.13, Target  1.56 / 1.74 / 2.1 / 2.45 / 2.9
             reply_msg = f"{coin}: Current Price {format_number(current_price)}, Resistant prices: {', '.join(resistance_support_levels[0:3])}"
             return reply_msg
@@ -2500,7 +2500,7 @@ def get_support_prices(symbol, interval = '1w'):
         current_price = float(df['Close'].iloc[-1])
         if current_price > 0:
             resistance_support_levels = calculate_resistance_support(df)
-            resistance_support_levels = [f"{format_number(price)}({format_number(100 * float(price) / current_price)}%)" for price in resistance_support_levels if price <= current_price * 0.9]
+            resistance_support_levels = [f"{format_number(price)}({format_number(100 * (float(price) - current_price) / current_price)}%)" for price in resistance_support_levels if price <= current_price * 0.9]
             # Reorder the list from high to low
             resistance_support_levels = resistance_support_levels[::-1]
             reply_msg = f"{coin}: Current Price {format_number(current_price)}, Support prices: {', '.join(resistance_support_levels[0:3])}"
